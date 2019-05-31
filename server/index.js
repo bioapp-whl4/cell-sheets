263a20c03d3df308ffb9facc0fc3cee60a51f6d3
Nathan Simpson <ndsimpson01@gmail.com>
2019-05-31 15:52:21 -0600

    Created server w/ register and register component

---
+++ server/index.js
@@ -0,0 +1,33 @@
+require('dotenv').config()
+const massive = require('massive')
+const session = require('session')
+const express = require('express')
+const controller = require('./controller')
+const { CONNECTION_STRING, SESSION_SECRET, SERVER_PORT } = process.env
+const app = express()
+app.use(express.json())
+app.use(session({
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 1000*60*60*24
+    }
+}))
+
+massive(CONNECTION_STRING)
+.then( db => {
+    app.set('db', db)
+    console.log(`DATABASE: Connected`)
+    app.listen(SERVER_PORT, () => {
+        console.log(`SERVER_PORT: ${SERVER_PORT}`)
+    })
+})
+.catch(
+    console.log(`Server down!`)
+)
+
+app.post('/auth/login',controller.login)
+app.post('/auth/register',controller.register)
+app.get('/auth/logout',controller.logout)
+app.get('/auth/session',controller.session)
