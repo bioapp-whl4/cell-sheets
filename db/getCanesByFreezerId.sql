2561e9fb74f9822f8f2bd5ab979b27c4254d5770
norm warren <normpw@gmail.com>
2019-06-04 15:20:55 -0600

    sql change for Jeff

--- db/getCanesByFreezerId.sql
+++ db/getCanesByFreezerId.sql
@@ -1,8 +1,7 @@
 SELECT distinct  
-c.id, c.name as cane FROM 
+f.id as freezer_id, c.id as cane_id, c.name as cane FROM 
 ba.freezer f
 JOIN ba.hierarchy h ON h.parent_id = 3
 JOIN ba.cane c ON c.hierarchy_id = h.hierarchy_id
 JOIN ba.freezer_type ft ON ft.freezer_id = f.id
 WHERE f.id =${freezer_id}
-returning f.id as freezer_id
