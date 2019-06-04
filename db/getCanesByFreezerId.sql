d418b100f614549cc8d5db9f4cbee5773051cc7f
norm warren <normpw@gmail.com>
2019-06-04 15:14:16 -0600

    added freezer_id to sql

--- db/getCanesByFreezerId.sql
+++ db/getCanesByFreezerId.sql
@@ -5,3 +5,4 @@ JOIN ba.hierarchy h ON h.parent_id = 3
 JOIN ba.cane c ON c.hierarchy_id = h.hierarchy_id
 JOIN ba.freezer_type ft ON ft.freezer_id = f.id
 WHERE f.id =${freezer_id}
+returning f.id as freezer_id
