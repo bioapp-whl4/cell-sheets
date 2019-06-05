a855e259c646f61c4768891d483bb09395eae485
Nathan Simpson <ndsimpson01@gmail.com>
2019-06-05 14:22:45 -0600

    updated db.getFreezers to prevent duplicate freezers being returned

--- src/Components/BurgerMenu/BurgerMenu.jsx
+++ src/Components/BurgerMenu/BurgerMenu.jsx
@@ -3,8 +3,33 @@ import { slide as Menu } from 'react-burger-menu'
 import {decorator as reduxBurgerMenu} from 'redux-burger-menu';
 import {action as toggleMenu} from 'redux-burger-menu';
 import store from '../../redux/store'
+import {connect} from 'react-redux'
+import axios from 'axios'
 
 class BurgerMenu extends Component {
+    state = {
+        freezers: [],
+        canes: [],
+        boxes: [],
+        grids: []
+    }
+
+    componentWillUpdate(){
+    }
+    
+    getFreezers = async () => {
+        let res = await axios.get('/api/freezers')
+        this.setState({ freezers: res.data })
+        this.props.updateFreezers(res.data)
+    }
+    getFreezerCane = async (freezer_id) => {
+        let res = await axios.get(`/api/freezer/canes?id=${freezer_id}`)
+        this.setState({ freezer_id: freezer_id, cane_id: '', freezercanes: res.data })
+    }
+    getFreezerBox = async (cane_id) => {
+        let res = await axios.get(`/api/cane/boxes?id=${cane_id}`)
+        this.setState({ freezerboxes: res.data, cane_id: cane_id })
+    }
 
     toggleBurger = () => {
         console.log('firing')
@@ -12,7 +37,7 @@ class BurgerMenu extends Component {
     }
 
   render () {
-      console.log(this.props)
+      console.log(`render`, this.props)
     return (
         <div className='burger-menu'>
         <button onClick={this.toggleBurger}>Toggle Burger Menu</button>
@@ -26,5 +51,10 @@ class BurgerMenu extends Component {
   }
 }
 
-export default reduxBurgerMenu(BurgerMenu)
+const mapStateToProps = (reduxState) => {
+    const { freezers, freezercanes, freezerboxes, boxes } = reduxState.reducer
+    return { freezers, freezercanes, freezerboxes, boxes } 
+}
+
+export default connect(mapStateToProps, null)(reduxBurgerMenu(BurgerMenu))
 
