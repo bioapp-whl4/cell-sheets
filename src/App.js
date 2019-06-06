a827cda8b34b585cda7f26cab6190b79e13c85b2
norm warren <normpw@gmail.com>
2019-06-06 11:43:34 -0600

    add search results test page

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
-import React from 'react';
-import router from './router';
-import {HashRouter} from 'react-router-dom';
+import React from "react";
+import router from "./router";
+import { HashRouter } from "react-router-dom";
 import store from "./redux/store";
 import { Provider } from "react-redux";
-import './App.scss'
-
+import "./App.scss";
 
 function App() {
   return (
     <Provider store={store}>
       <HashRouter>
-        <div>
-          {router}
-         
-        </div>
+        <div>{router}</div>
       </HashRouter>
     </Provider>
   );
---
+++ src/Components/Search/HeaderSearch.jsx
@@ -0,0 +1,67 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
+import { updateSamples } from "../../redux/auth.reducer";
+import { Link } from "react-router-dom";
+import axios from "axios";
+class HeaderSearch extends Component {
+  constructor() {
+    super();
+    this.state = {
+      filterValue: "",
+      samples: []
+    };
+  }
+  async componentDidMount() {
+    await this.getSamples();
+  }
+  getSamples = async () => {
+    let res = await axios.get("/api/samples");
+    this.setState({ samples: res.data });
+    this.props.updateSamples(res.data);
+  };
+  // track user inputs via local state
+  handleInput = event => {
+    let { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  render() {
+    let displaySamples = this.state.samples.map((elem, i) => {
+      return (
+        <Link to={`/api/test`}>
+          <div key={i}>
+            <h3>{elem.freezer_name}</h3>
+            <i class="fas fa-temperature-low" />
+            <h4>{elem.temperature}</h4>
+            <h4>{elem.freezer_type}</h4>
+          </div>
+        </Link>
+      );
+    });
+    return (
+      <div className="display">
+        <div className="contents">
+          <h4 className="category">Samples</h4>
+          <i class="fas fa-snowflake cold" />
+          <div className="displayContents">{displaySamples}</div>
+        </div>
+      </div>
+    );
+  }
+}
+const mapStateToProps = reduxState => {
+  const { user_id, samples, authenticated } = reduxState;
+  return { user_id, samples, authenticated };
+};
+
+const mapDispatchToProps = {
+  updateSamples
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(withRouter(HeaderSearch));
