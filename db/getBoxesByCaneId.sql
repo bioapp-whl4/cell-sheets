3cc938b70eda52717816159aa7362ba41439a192
norm warren <normpw@gmail.com>
2019-06-10 14:47:37 -0600

    changed sql

--- db/getBoxesByCaneId.sql
+++ db/getBoxesByCaneId.sql
@@ -2,4 +2,4 @@ SELECT c.id as cane_id, b.id as box_id, b.name as box_name
 from ba.box b
 JOIN ba.hierarchy h ON h.parent_id = 4
 JOIN ba.cane c ON c.hierarchy_id = h.parent_id
-WHERE c.id = ${cane_id}
+WHERE b.cane_id = ${cane_id}
