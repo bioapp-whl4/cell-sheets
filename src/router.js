35515bbf33efcd274d409ac298d808d459a81b40
Jeoffrey <jeoff.howe@gmail.com>
2019-06-11 14:51:28 -0600

    fixed router merge

--- src/router.js
+++ src/router.js
@@ -4,18 +4,8 @@ import Login from "./Components/login";
 import Register from "./Components/Register";
 import Dashboard from "./Components/Dashboard/Dashboard";
 import AddLocation from "./Components/AddLocation/AddLocation";
-<<<<<<< HEAD
-import HeaderSearch from "./Components/Search/HeaderSearch";
-import Filter from './Components/AdvanceSearch/AdvanceSearch';
-import AddSpecimen from './Components/Add Specimen/AddSpecimen';
-import DisplayFreezers from './Components/DisplayInventory/Sub_components/DisplayFreezers/DisplayFreezers'
-import FreezerCane from './Components/DisplayInventory/Sub_components/FreezerCane/FreezerCane'
-import FreezerBox from './Components/DisplayInventory/Sub_components/FreezerBox/FreezerBox'
-
-=======
 import Filter from "./Components/AdvanceSearch/AdvanceSearch";
 import AddSpecimen from "./Components/Add Specimen/AddSpecimen";
->>>>>>> master
 
 // Test Hierarchy
 import Hierarchy from "./Components/DisplayInventory/Hierarchy";
